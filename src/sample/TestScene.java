df5609843b0a67e41a986f74d5699b3d04867b8a
raviiist <itsravi.isro@gmail.com>
2018-10-08 17:59:57 +0530

    allinone source code to be uploaded to github

--- src/sample/TestScene.java
+++ src/sample/TestScene.java
@@ -19,7 +19,7 @@ public class TestScene {
     void switch2Scene1(ActionEvent event) throws IOException {
         Parent scene1Parent = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Scene scene1 = new Scene(scene1Parent);
-
+        System.out.println("Switching scenes...");
         Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
         window.setScene(scene1);
         window.show();
