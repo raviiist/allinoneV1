ab31b980b56c62948c95c3b16460bd1773acaca7
raviiist <itsravi.isro@gmail.com>
2018-10-26 19:20:27 +0530

    allinone source code to be uploaded to github

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.nio.file.Path;
@@ -18,6 +19,9 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("All In One");
         primaryStage.setScene(new Scene(root));
+        //to set the application icon
+        primaryStage.getIcons().add(new Image("file:resources/images/icons8-gps-antenna-96.png"));
+        //Source:   https://code.makery.ch/library/javafx-tutorial/part4/
 
         primaryStage.show();
     }
--- src/sample/TestScene.java
+++ src/sample/TestScene.java
@@ -10,16 +10,25 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import java.io.IOException;
-import java.util.Optional;
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.CREATE;
 
 public class TestScene {
     @FXML
     private Button scene2b1;
     @FXML
     private Hyperlink closeLinkscene1;
-
+    @FXML
+    private TextField cmdLine;
+    @FXML
+    private TextArea textOut;
 
     @FXML
     void switch2Scene1(ActionEvent event) throws IOException {
@@ -31,6 +40,91 @@ public class TestScene {
         window.show();
 
     }
+    @FXML
+    void executeCmd(ActionEvent event) {
+        String cmdline=cmdLine.getText();
+        String[] cmds = {cmdline,"-send"};
+        String[] cmds1 = {"/bin/sh", "-c",cmdline};     //ping -c 3 google.com      Use this cmd :)
+        System.out.println("Command: "+cmdline);
+        //Here is one method...
+        /*try {
+            Process p = Runtime.getRuntime().exec(cmds1);
+            p.waitFor();
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+                //System.out.println("Output:");
+                String s = null;
+                while ((s = stdInput.readLine()) != null) {
+                    System.out.println(s);
+                    textOut.appendText(s+"\n");
+                }
+                //System.out.println("Output(if any error):");
+                while ((s = stdError.readLine()) != null) {
+                    System.out.println(s);
+                    textOut.appendText(s+"\n");
+                }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        //Here is another method(better)...
+        ProcessBuilder builder = new ProcessBuilder(cmds1);
+        builder.redirectErrorStream(true);
+
+        Process process = null;
+        try {
+            process = builder.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+                /*InputStream is = process.getInputStream();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                String line = null;
+
+                try {
+                while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                textOut.appendText(line+"\n");
+                }
+                } catch (IOException e) {
+                e.printStackTrace();
+                //Logger.getLogger(Documents.class.getName()).log(Level.SEVERE, null, e);
+                }*/
+                Path outStream = Paths.get("/home/ravi/temp1.txt");
+                BufferedReader reader1 = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line1 = null;
+                //BufferedWriter writer2 = new BufferedWriter(new OutputStreamWriter(Files.newOutputStream(outStream,CREATE, APPEND)));
+        try {
+            while ((line1 = reader1.readLine()) != null) {
+                System.out.println(line1);
+                //writer2.write("\n" + line1, 0, line1.getBytes().length);
+                //textOut.appendText(line1+"\n");
+                PrintStream printStream = new PrintStream(new CustomOutputStream(textOut));
+                System.setOut(printStream);
+                System.setErr(printStream);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        cmdLine.clear();
+    }
+
+    class CustomOutputStream extends OutputStream {
+        public CustomOutputStream(TextArea textArea) {
+        textOut = textArea;
+        }
+        @Override
+        public void write(int b) throws IOException {
+        // redirects data to the text area
+        textOut.appendText(String.valueOf((char)b));
+        // scrolls the text area to the end of data
+        //textOut.setCaretPosition(textOut.getDocument().getLength());
+        }
+    }
+
     @FXML
     void closeScene1(ActionEvent event) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
@@ -70,3 +164,12 @@ public class TestScene {
         System.out.println(alert.getResult().equals(buttonTypeOne));
     }
 }
+
+
+
+//NOTE:
+/* /usr/bin/libreoffice --headless --convert-to pdf:'writer_pdf_Export' --outdir /home/ravi/Documents/ /home/ravi/Documents/Untitled1.txt
+*  host -t a google.com
+ * ping -c 3 google.com
+  *
+  * */
